c0b1128f53dabda04cb716be05620efbde45eb42
Nekika <hugo.jahnke@gmail.com>
2020-07-01 12:28:54 +0200

    Utils - Output - Implementing methods - add_* methods : used to append a field to the right section - parse : used to parse the structure into valid JSON - save : call the parse method and write the output file

--- utils/output.py
+++ utils/output.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 
 class Output:
@@ -21,3 +22,38 @@ class Output:
                 "selectBox": []
             }
         }
+
+    def add_inputText(self, field):
+        inputText_list = self.structure["form"]["inputText"]
+        inputText_list.append(field)
+
+    def add_inputNumber(self, field):
+        inputNumber_list = self.structure["form"]["inputNumber"]
+        inputNumber_list.append(field)
+
+    def add_inputDate(self, field):
+        inputDate_list = self.structure["form"]["inputDate"]
+        inputDate_list.append(field)
+
+    def add_inputRange(self, field):
+        inputRange_list = self.structure["form"]["inputRange"]
+        inputRange_list.append(field)
+
+    def add_textArea(self, field):
+        textArea_list = self.structure["form"]["textArea"]
+        textArea_list.append(field)
+
+    def add_selectBox(self, field):
+        selectBox_list = self.structure["form"]["selectBox"]
+        selectBox_list.append(field)
+
+    def parse(self):
+        return json.dumps(self.structure)
+
+    def save(self):
+        parsed_structure = self.parse()
+        f = open(self.path, "w")
+        f.write(parsed_structure)
+        f.close()
+
+
