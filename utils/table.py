f4ada8a6222a166d613370cb76397908d60d4350
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-07-07 15:43:39 +0200

    Utils - Editing table items & checkbox
    - For this usage, it's better to use QTableWidgetItem because we are able to catch the itemChanged event
    - Checkboxes are now created with an attribute arg in order to send it whenever itemChanged event is fired. Here we use it to know which checkbox was changed

--- utils/table.py
+++ utils/table.py
@@ -1,16 +1,23 @@
-from qgis.PyQt.QtWidgets import QLabel, QCheckBox
+from qgis.PyQt.QtWidgets import QTableWidgetItem
+from qgis.PyQt.QtCore import Qt
 
 def create_label_item(title):
-    return QLabel(title)
+    item = QTableWidgetItem(title)
+    item.setTextAlignment(Qt.AlignCenter)
+    return item
 
-def create_checkbox_item():
-    return QCheckBox()
+def create_checkbox_item(attribute):
+    item = QTableWidgetItem()
+    item.setFlags(Qt.ItemIsUserCheckable | Qt.ItemIsEnabled)
+    item.setCheckState(Qt.Unchecked)
+    item.setData(1, attribute)
+    return item
 
 def create_row(field):
     return dict(
         label=create_label_item(field["name"]),
-        checkbox_hidden=create_checkbox_item(),
-        checkbox_disabled=create_checkbox_item()
+        checkbox_hidden=create_checkbox_item('hidden'),
+        checkbox_disabled=create_checkbox_item('disabled')
     )
 
 
@@ -21,6 +28,6 @@ def create_rows(fields):
 def fill_table(table, fields):
     rows = create_rows(fields)
     for index, row in enumerate(rows):
-        table.setCellWidget(index, 0, row["label"])
-        table.setCellWidget(index, 1, row["checkbox_disabled"])
-        table.setCellWidget(index, 2, row["checkbox_hidden"])
+        table.setItem(index, 0, row["label"])
+        table.setItem(index, 1, row["checkbox_disabled"])
+        table.setItem(index, 2, row["checkbox_hidden"])
